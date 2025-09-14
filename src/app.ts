ae2691f0d7cabee7bd608044167a41e79cfe6d6f
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-09-14 15:56:07 -0300

    update: in the app file to add the App class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import express, { type Application } from 'express';
-import { loginRoutes } from './routes/login.route';
-import { userRoutes } from './routes/user.routes';
+import express, { Express } from 'express';
+// import { loginRoutes } from './routes/login.route';
+// import { userRoutes } from './routes/user.routes';
 
-const app: Application = express();
-app.use(express.json());
+// const app: Application = express();
+// app.use(express.json());
 
-app.use('/login', loginRoutes);
-app.use('/user', userRoutes);
+// app.use('/login', loginRoutes);
+// app.use('/user', userRoutes);
 
-export { app };
+// export { app };
+
+class App {
+    public app: Express;
+
+    constructor() {
+        this.app = express();
+        this.middlerwares();
+        this.routes();
+    }
+
+    middlerwares() {
+        this.app.use(express.json());
+    }
+
+    routes() {}
+}
+
+export default new App().app;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { app } from './app';
+import app from './app';
 import 'dotenv/config';
 import prisma from './prisma/client';
 
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 3000;
 const runningMsg: string = `App is listening on port ${PORT}`;
 
 const runServer = async () => {
-  try {
-    await prisma.$connect();
-    app.listen(PORT, () => {
-      console.log(runningMsg);
-    });
-  } catch (error) {
-    console.error('Failed to start server:', error);
-    process.exit(1);
-  }
+    try {
+        await prisma.$connect();
+        app.listen(PORT, () => {
+            console.log(runningMsg);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
 };
 
 runServer();
