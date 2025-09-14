71bbc8ec9c6d2ba586acbee6dac3a38241894f96
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-09-14 18:12:18 -0300

    feat: create user migration

--- prisma/migrations/20250914210555_create_user_table/migration.sql
+++ prisma/migrations/20250914210555_create_user_table/migration.sql
@@ -4,12 +4,15 @@ CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN');
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "nickname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "profile_image_url" TEXT NOT NULL,
+    "bio" TEXT NOT NULL,
+    "premium" BOOLEAN NOT NULL DEFAULT false,
     "role" "public"."Role" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
